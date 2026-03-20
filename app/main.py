fd5848fcefe17e43eb08d1e380cf1559fb3142cd
osmarsalesjr <osmarsalesjr@gmail.com>
2026-03-19 21:25:51 -0300

    feature: adicionar estrutura inicial do projeto

---
+++ app/main.py
@@ -0,0 +1,28 @@
+def main():
+    pause()
+    show_menu()
+    pass
+
+def pause():
+    input("Pressione ENTER para continuar...")
+
+def show_menu():
+    print("Menu em construção")
+
+def add_tool():
+    pass
+
+def remove_tool():
+    pass
+
+def list_tools():
+    pass
+
+def list_closed_boxes():
+    pass
+
+def create_final_report():
+    pass
+
+if __name__ == "__main__":
+    main()
